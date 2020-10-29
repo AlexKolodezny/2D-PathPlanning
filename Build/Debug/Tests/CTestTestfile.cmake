8bb263511ab462a1611c5b8ce3a19b1576e5c312
alexkolodezny <alexkolodezny@gmail.com>
2020-10-29 14:41:53 +0300

    first commit

---
+++ Build/Debug/Tests/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexander/Documents/hse/year2/2D-PathPlanning/Tests
+# Build directory: /home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test1 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/0_test.xml")
+add_test(Test2 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/1_test.xml")
+add_test(Test3 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/2_test.xml")
+add_test(Test4 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/3_test.xml")
+add_test(Test5 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/4_test.xml")
+add_test(Test6 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/5_test.xml")
+add_test(Test7 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/6_test.xml")
+add_test(Test8 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/7_test.xml")
+add_test(Test9 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/8_test.xml")
+add_test(Test10 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/9_test.xml")
+add_test(Test11 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/A_test.xml")
+add_test(Test12 "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Debug/Tests/testbench" "../../../TestData/B_test.xml")
---
+++ Build/Release/Src/CMakeFiles/PPLIB.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/config.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/config.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/environmentoptions.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/environmentoptions.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/map.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/map.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/mission.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/mission.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/search.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/search.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/tinyxml2.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/tinyxml2.cpp.o"
+  "/home/alexander/Documents/hse/year2/2D-PathPlanning/Src/xmllogger.cpp" "/home/alexander/Documents/hse/year2/2D-PathPlanning/Build/Release/Src/CMakeFiles/PPLIB.dir/xmllogger.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
