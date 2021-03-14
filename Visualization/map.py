f8bd2a38dbcf88a0d93ddde372874655541b5239
AlexKolodezny <alexkolodezny@gmail.com>
2021-03-14 22:43:06 +0300

    Visualization improve

--- Visualization/map.py
+++ Visualization/map.py
@@ -1,6 +1,7 @@
 import sys
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
+from matplotlib.widgets import Slider
 
 if len(sys.argv) < 2:
     print("Error! Filename is required!")
@@ -30,13 +31,51 @@ for log in root.findall("log"):
 else:
     print("Error! Tag 'paths' not found!")
     sys.exit(1)
+
+lppaths = []
+
+paths_length = []
+paths_danger = []
     
 for item in paths.findall("item"):
-    path = item.find("lplevel")
-    for node in path.findall("node"):
-        x = int(node.get("x"))
-        y = int(node.get("y"))
-        grid[y][x] = 2
+    path_summary = item.find("pathsummary")
+    paths_length.append(float(path_summary.get("length")))
+    paths_danger.append(float(path_summary.get("danger")))
+    path_tag = item.find("lplevel")
+    path = []
+    for node in path_tag.findall("node"):
+        path.append((int(node.get("y")), int(node.get("x"))))
+    lppaths.append(path)
 
-plt.imshow(grid)
+fig = plt.figure(constrained_layout = True)
+spec = fig.add_gridspec(ncols = 2, nrows = 2, height_ratios = [10, 1])
+ax1 = fig.add_subplot(spec[0, 0])
+ax2 = fig.add_subplot(spec[0, 1])
+ax_silder = fig.add_subplot(spec[1, :])
+
+slider = Slider(ax_silder, "Path", 0, 1, valinit = 0)
+
+cur_num = 0
+for coor in lppaths[cur_num]:
+    grid[coor[0]][coor[1]] = 2
+
+im = ax1.imshow(grid)
+ax2.plot(paths_length, paths_danger, "b.")
+l, = ax2.plot(paths_length[cur_num], paths_danger[cur_num], "ro")
+ax2.grid()
+
+def update(val):
+    global cur_num
+    for coor in lppaths[cur_num]:
+        grid[coor[0]][coor[1]] = 0
+    cur_num = int(val * (len(lppaths) - 1))
+    for coor in lppaths[cur_num]:
+        grid[coor[0]][coor[1]] = 2
+    
+    im.set_array(grid)
+    l.set_xdata(paths_length[cur_num])
+    l.set_ydata(paths_danger[cur_num])
+    fig.canvas.draw()
+    
+slider.on_changed(update)
 plt.show()
